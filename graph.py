235c64d1eb7943c279857694420bd80f5193d7e6
mdkfm <2119104071@qq.cpm>
2023-01-11 23:47:25 +0800

    skf

--- graph.py
+++ graph.py
@@ -3,52 +3,62 @@ class Edges():
 		'''
 		has_direction仅限关键字参数确定边是否有向，默认无向
 		'''
-		self.edges = set()
-		self.origin_edges = []
+		self.edges = []
 		self.has_direction = has_direction
 		if edges:
 			for edge in edges:
-				self.origin_edges.append(edge)
-				if self.has_direction:
-					self.edges.add(edge)
-				else:
-					self.edges.add(frozenset(edge))
+				self.edges.append(edge)
 	
 	def __contains__(self, edge):
-		return frozenset(edge) in self.edges or edge in self.edges
+		if self.has_direction:
+			return edge in self.edges
+		else:
+			u, v = edge
+			return edge in self.edges or (v, u) in self.edges
+	
+	def __call__(self):
+		return self.edges
 	
 	def __len__(self):
-		return len(self.origin_edges)
+		return len(self.edges)
 	
 	def __iter__(self):
-		yield from self.origin_edges
+		yield from self.edges
 		
 	def add(self, *edges):
 		for edge in edges:
-			self.origin_edges.append(edge)
-			if self.has_direction:
-				self.edges.add(edge)
-			else:
-				self.edges.add(frozenset(edge))
-
+			self.edges.append(edge)
+		
+	def cut(self, *edges):
+		if self.has_direction:
+			for edge in edges:
+				self.edges.remove(edge)
+		else:
+			for edge in edges:
+				try:
+					self.edges.remove(edge)
+				except:
+					u, v = edge
+					self.edges.remove((v, u))
+					
+					
 class Graph():
-	def __init__(self, vertex_num: 'int', edges: 'Edges'):
-		'''
-		vertex_num为顶点数， 顶点编号从1至vertexs
-		'''
-		self.vertex_num = vertex_num
+	def __init__(self, vertexs: 'list', edges: 'Edges'):
+		self.vertexs = {num:vertex for num, vertex in enumerate(vertexs)}
+		self.vertex_id = {vertex:num for num, vertex in enumerate(vertexs)}
+		self.vertex_num = len(vertexs)
 		self.edges = edges
 		self.edge_num = len(edges)
 	
 	def ad_matrix(self):
 		'''
-		返回图的邻接矩阵
+		return the adjacent matrix of the graph by the order of self.vertexs
 		'''
 		adjacency_matrix = [[0 for i in range(self.vertex_num)] for i in range(self.vertex_num)]
-		for i in range(1, self.vertex_num+1):
-			for j in range(1, self.vertex_num+1):
-				if (i, j) in self.edges:
-					adjacency_matrix[i-1][j-1] = 1
+		for i in range(self.vertex_num):
+			for j in range(self.vertex_num):
+				if (self.vertexs[i], self.vertexs[j]) in self.edges:
+					adjacency_matrix[i][j] = 1
 		return adjacency_matrix
 		
 	def ad_list(self, vertex):
@@ -56,61 +66,89 @@ class Graph():
 		return a list of adjacent vertexies
 		'''
 		adjacent_list = []
-		for u in range(1, self.vertex_num+1):
-			if (vertex, u) in self.edges:
-				adjacent_list.append(u)
+		for u in range(self.vertex_num):
+			if (vertex, self.vertexs[u]) in self.edges:
+				adjacent_list.append(self.vertexs[u])
 		
 		return adjacent_list
 	
 	def max_degree(self):
-		#如果边有向则转化为无向，再用无向图的邻接矩阵求最大度
-		graph = self
 		if self.edges.has_direction:
-			edges = Edges(*self.edges.origin_edges)
-			graph = Graph(self.vertexs, edges)
-		return max(sum(list) for list in graph.ad_matrix())
+			edges = Edges(self.edges, has_direction=False)
+			graph = Graph(self.vertexs.values(), edges)
+		return max(sum(l) for l in graph.ad_matrix())
 	
-	def add_vertex(self, vertex_num):
-		self.vertex_num += vertex_num
+	def add_vertex(self, vertexs):
+		for vertex in vertexs:
+			num = len(self.vertexs)
+			self.vertexs[num] = vertex
+		self.vertex_num += len(vertexs)
+	
+	# ~ def cut_vertex(self, vertexs):
 	
 	def add_edge(self, *edges):
 		self.edges.add(*edges)
-		
+	
+	def cut_edge(self, *edges):
+		self.edges.cut(*edges)
+	
 	def has_circle(self):
 		'''
-		stackdfs
+		find circles in the graph by degree
 		'''
-		if not seld.edges.has_direction and len(self.origin_edges) != set(self.oringin_edges):
+		edges = Edges(*self.edges(), has_direction=self.edges.has_direction)
+		c = Graph(self.vertexs.values(), edges)
+		degree = [0 for i in range(c.vertex_num)]
+		if c.edges.has_direction:
+			for edge in c.edges():
+				_, v = edge
+				degree[c.vertex_id[v]] += 1
+			while 0 in degree:
+				for i in range(c.vertex_num):
+					if degree[i] == 0:
+						for u in c.ad_list(self.vertexs[i]):
+							c.cut_edge((self.vertexs[i],u))
+							degree[self.vertex_id[u]] += -1
+							degree[i] = -1
+		else:
+			for edge in c.edges():
+				u, v = edge
+				degree[c.vertex_id[u]] += 1
+				degree[c.vertex_id[v]] += 1
+			while 1 in degree:
+				for i in range(c.vertex_num):
+					if degree[i] == 1:
+						for u in c.ad_list(self.vertexs[i]):
+							c.cut_edge((self.vertexs[i],u))
+							degree[self.vertex_id[u]] += -1
+							degree[i] = -1
+		# ~ for n in range(c.vertex_num):
+			# ~ for i in range(c.vertex_num):
+				# ~ degree = 0
+				# ~ edges = []
+				# ~ for j in range(c.vertex_num):
+					# ~ if j != i and c.vertexs[i] in c.ad_list(self.vertexs[j]):
+						# ~ degree += 1
+						# ~ edges.append((c.vertexs[j], self.vertexs[i]))
+				# ~ if edges:
+					# ~ if not c.edges.has_direction and degree == 1:
+						# ~ c.cut_edge(*edges)
+					# ~ elif c.edges.has_direction and degree == 0:
+						# ~ c.cut_edge(*edges)
+		if len(c.edges):
 			return True
-		for edge in self.edges:
-			u, v = edge
-			if (v, u) in self.edges:
-				return True
-		visited_v = [0 for vertex in range(self.vertex_num)]
-		for i in range(1, self.vertex_num+1):
-			if visited[i-1] == 0:
-				s = []
-				instack = [0 for vertex in range(self.vertex_num)]
-				s.append(i)
-				instack[i-1] = 1
-				while s:
-					c = s.pop()
-					instack[c-1] = 0
-					visited[c-1] = 1
-					for u in self.ad_list(c):
-						if visited[u] == 0:
-							s.append(u)
-							instack[u-1] = 1
-						elif instack[u-1] == 1:
-							return True
-		return False
-	
-class weight_edgraph(Graph):
-	def __init__(self, vertex_num:'int', edges:'set of sets', weights:'dict'):
-		self.vertex_num = vertex_num
+		else:
+			return False
+		
+
+class weighted_graph(Graph):
+	def __init__(self, vertexs:'list', edges:'Edges', weights:'dict'):
+		self.vertexs = {num:vertex for num, vertex in enumerate(vertexs)}
+		self.vertex_id = {vertex:num for num, vertex in enumerate(vertexs)}
+		self.vertex_num = len(vertexs)
 		self.edges = edges
 		self.weights = weights
 	
-	def add_weights(self, weights):
+	def add_weight(self, weights):
 		for edge, weight in weights:
 			self.weights[edge] = weight
--- kruskal.py
+++ kruskal.py
@@ -1,18 +1,15 @@
-from graph import Graph, Edges, weigh_tedgraph
+from graph import Graph, Edges, weighted_graph
 
 def kruskal(wgraph):
 	edges = [edge for edge in wgraph.weights.keys()]
 	edges.sort(key=lambda edge: wgraph.weights[edge])
 	
 	edges1 = Edges()
-	span_tree = weighted_graph(wgraph.vertex_num, edges1, {})
-	vertexs = set()
+	span_tree = weighted_graph(wgraph.vertexs.values(), edges1, {})
 	
 	for edge in edges:
-		span_tree_i = span_tree.add_edges(edge)
-		span_tree_i.add_weights(wgraph.weights[edge])
-		if not span_tree_i.has_circle():
-			span_tree = span_tree_i
-	
+		span_tree.add_edge(edge)
+		if span_tree.has_circle():
+			span_tree.cut_edge(edge)
 	return span_tree
 
